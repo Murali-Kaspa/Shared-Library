20c7ab3446767b652bc26282c68c88c4011992af
Murali-Kaspa <murali.kaspa26@gmail.com>
2025-11-08 14:52:31 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,9 +1,7 @@
 def call(){
 def date = sh(script: 'date', returnStdout: true).trim()
 echo "Hello Murali, Current date is : ${date}"
-sh '''
-sudo yum install docker -y
-sudo service docker start
-docker run -d -p 80:80 myimage --nginx
+echo "sudo yum install docker -y"
+echo "sudo service docker start"
 '''
 }
